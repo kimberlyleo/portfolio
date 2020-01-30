0466d427cda431b93319fe6ac25eb6cd3afd0854
Master <Master@macbook-pro.home>
2020-01-29 23:59:44 -0800

    react-portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -8,33 +8,29 @@ export default class About extends Component {
 
             <div className="three columns">
 
-               <img className="profile-pic"  src="images/profile-pic.jpg" alt="profile picture" />
+               <img className="profile-pic"  src="images/profile-pic.jpg" alt="profile" />
 
             </div>
 
             <div className="nine columns main-col">
 
                <h2>ABOUT ME</h2>
-               <p>
-               {
-                 resumeData.aboutme
-               }
-               </p>
-
-               <div className="row">
+                <p>{resumeData.aboutme}</p>
+                  <div className="row">
 
                   <div className="columns contact-details">
-
                   <h2>CONTACT DETAILS</h2>
                   <p className="address">
        						<span>{resumeData.name}</span>
-                     <br></br>
-       						   <span>
+
+       						   <div>
                      {resumeData.address}
-                    </span>
-                    <br></br>
-                    <span>{resumeData.email}</span>
-       					   </p>
+                    </div>
+
+                    <div><a href={"mailto:kimeleonard@gmail.com"}>{resumeData.email}</a></div>
+                    <div><a href="https://www.linkedin.com/kimberly-leo" target="_blank">linkedin.com/kimberlyleo</a></div>
+                   </p>
+
                   </div>
                </div>
             </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,9 +19,9 @@ export default class Footer extends Component {
               })
             }
           </ul>
-          
+
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#"><i className="icon-up-open" /></a></div>
       </div>
     </footer>
     );
