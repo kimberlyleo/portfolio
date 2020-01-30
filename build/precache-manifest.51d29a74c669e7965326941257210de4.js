1659d305ea166aeff3d32e98213093536c4f758b
kimberlyleo <kimeleonard@gmail.com>
2020-01-30 09:49:04 -0800

    deploy readywith build dir

---
+++ build/precache-manifest.51d29a74c669e7965326941257210de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1b0122a87778d92f8b1",
+    "url": "/static/css/main.f3da1bc4.chunk.css"
+  },
+  {
+    "revision": "a1b0122a87778d92f8b1",
+    "url": "/static/js/main.a1b0122a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e5e79dc8c0216d0a89ec11998b4d5d5",
+    "url": "/index.html"
+  }
+];
